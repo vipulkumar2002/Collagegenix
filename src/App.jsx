fcbfbc541418f161056adf0ae8e7b77cfee91d67
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-02-10 10:58:11 +0530

    setup all the file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navabar from "./Components/Navbar/Navabar";
+export const App = () => {
+  return (
+    <>
+      <h1>Hii this is home page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores,
+        deserunt incidunt commodi quis at nostrum ratione dicta in reprehenderit
+        quo dolorum hic ab possimus facilis repellat odio officiis minima magni?
+      </p>
+      <Navabar />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Navbar/Navabar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+export const Navabar = () => {
+  return (
+    <>
+      <h1 className="Heading">Vipul</h1>
+    </>
+  );
+};
+
+export default Navabar;
