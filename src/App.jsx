658764313e1449e77452541bcf888dd9f2410578
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-02-10 23:19:11 +0530

    updated file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import Navabar from "./Components/Navbar/Navabar";
+import Navabar from "./Pages/Navbar/Navabar";
 export const App = () => {
   return (
     <>
-      <h1>Hii this is home page</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores,
-        deserunt incidunt commodi quis at nostrum ratione dicta in reprehenderit
-        quo dolorum hic ab possimus facilis repellat odio officiis minima magni?
-      </p>
       <Navabar />
     </>
   );
---
+++ src/Pages/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+export const About = () => {
+  return (
+    <>
+      <h1 className="about-title">This is a about</h1>
+    </>
+  );
+};
+
+export default About;
