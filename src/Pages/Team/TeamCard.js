5ab3aedeeeb7f7f9ac56278c7add2c4d7649eb74
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-11-14 10:35:20 +0530

    redirect to new page fixed

--- src/Pages/Team/TeamCard.js
+++ src/Pages/Team/TeamCard.js
@@ -3,7 +3,8 @@ import "./TeamCard.css";
 // import { Link} from 'react-router-dom'
 
 function TeamCard(props) {
-  return (<>
+  return (
+    <>
       <div className="container">
         <img src={props.img} alt="" />
         <h5>{props.name}</h5>
@@ -20,23 +21,40 @@ function TeamCard(props) {
             <i className="fab fa-facebook-f"> </i>
           </a> */}
 
-    <div className='Team-icons'>
-            <a href="https://www.facebook.com/profile.php?id=100079480882277" className="fa-icon">
+          <div className="Team-icons">
+            <a
+              target="_blank"
+              href="https://www.facebook.com/profile.php?id=100079480882277"
+              className="fa-icon"
+            >
               <i className="fab fa-facebook-f"> </i>
             </a>
-            
-            <a href="https://www.instagram.com/vishal_patel_0803/" className="ins-icon">
+
+            <a
+              href="https://www.instagram.com/vishal_patel_0803/"
+              className="ins-icon"
+              target="_blank"
+            >
               <i className="fab fa-instagram"></i>
             </a>
-            <a  href="https://www.linkedin.com/in/vishal-patel-0937721a7/" className="lin-icon">
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/vishal-patel-0937721a7/"
+              className="lin-icon"
+            >
               <i className="fab fa-linkedin"></i>
             </a>
-            <a href="https://github.com/YVishal-Patel/" className="git-icon">
+            <a
+              target="_blank"
+              href="https://github.com/YVishal-Patel/"
+              className="git-icon"
+            >
               <i className="fab fa-github"></i>
             </a>
           </div>
-    </div>
-    </div>
-    </>)
+        </div>
+      </div>
+    </>
+  );
 }
 export default TeamCard;
